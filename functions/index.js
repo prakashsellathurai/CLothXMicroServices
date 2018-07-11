900b32f56384697c360d4a5c06aa33287351ccbd
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-07-11 15:06:36 +0530

    directory refactoring update

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,15 @@
 
-var admin = require('firebase-admin')
-var functions = require('firebase-functions')
+const admin = require('firebase-admin')
+const functions = require('firebase-functions')
 try { admin.initializeApp(functions.config()) } catch (e) { console.error(e) }
-
-var OncreateNewClothes = require('./firestore/clothes/OnCreateNewClothes')
-var OndeleteClothes = require('./firestore/clothes/OnDeleteClothes')
-var app = require('./Auth/Auth')
-var addEmployee = require('./firestore/store/employee/addemployee')
-var OncreateNewStore = require('./firestore/store/OnCreateNewStore')
+// ======================== firetsore triggers ======================================== //
+const OncreateNewStore = require('./firestore/store/OnCreateNewStore')
+const OncreateNewClothes = require('./firestore/clothes/OnCreateNewClothes')
+const OndeleteClothes = require('./firestore/clothes/OnDeleteClothes')
+// +++++++++++++++++++++++++ http triggers ++++++++++++++++++++++++++++++++++++++++++++ //
+const app = require('./https/Auth/Auth')
+const addEmployee = require('./https/employee/addemployee')
+// ------------------------- cloud function exports ----------------------------------- //
 exports.OncreateNewClothes = OncreateNewClothes
 exports.OndeleteClothes = OndeleteClothes
 exports.Login = app
