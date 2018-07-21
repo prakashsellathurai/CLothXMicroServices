c03dcbd88e6d608dcbbc97bfc7d17dff3626cceb
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-07-21 19:12:18 +0530

    filemove handler done prone to testing

--- test/UNITTEST/storage/filemove/filemover.js
+++ test/UNITTEST/storage/filemove/filemover.js
@@ -1,4 +1,5 @@
 var admin = require('firebase-admin')
+var CONTANTS = require('../../../../functions/environment/CONSTANTS')
 var gcloud = require('@google-cloud/storage')({
   projectId: 'clothxnet',
   keyFilename: './../../../../functions/environment/clothxnet-firebase-adminsdk-wkk1h-a27faaab6d.json'
@@ -8,15 +9,22 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://clothxnet.firebaseio.com'
 })
-var firestore = admin.firestore()
-const readline = require('readline')
-const stream = require('stream');
-var bucket = gcloud.bucket('clothxnet.appspot.com')
-function HandleFilemove (oldlocatoion, newlocation) {
-  var oldFile = bucket.file(oldlocatoion)
-  var newfile = bucket.file(newlocation)
-  var readFile = oldFile.createReadStream()
-  var writFile = newfile.createResumableUpload()
-  readFile.pipe(writFile)
+var storage = admin.storage().bucket(CONTANTS.STORAGE_BUCKET)
+function HandleFilemove (uuid, sid, imagelocationArr, logolocationarr) {
+  let promises = []
+  for (let index = 0; index < logolocationarr.length; index++) {
+    promises.push(storage.file(`/DbIndex/stores/addstorelog/${uuid}/${logolocationarr[index]}`).move(`stores/${sid}/${logolocationarr[index]}`))
+  }
+  for (let index = 0; index < imagelocationArr.length; index++) {
+    promises.push(storage.file(`/DbIndex/stores/addstorelog/${uuid}/${imagelocationArr[index]}`).move(`stores/${sid}/${imagelocationArr[index]}`))
+  }
+  return Promise.all(promises)
 }
-HandleFilemove('/log', '/newlocation')
+function CloudMove (oldlocation, newlocation) {
+return storage.file(oldlocation).move(newlocation)
+}
+CloudMove('/logs/addstore/aax4wj9B_700w_0.jpg','a.jpg').then((ref) =>{
+  console.log()
+}).catch(err => {
+  throw err
+})
