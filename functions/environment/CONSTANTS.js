bbd841c5c2d8bb9a4cbc91fe90ac98f5018cac32
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-09-01 19:33:48 +0530

    added test credentials

--- functions/environment/CONSTANTS.js
+++ functions/environment/CONSTANTS.js
@@ -11,5 +11,11 @@ module.exports = {
     action: 'read',
     expires: '03-09-2491'
   },
-  FIRESTORE_SETTINGS: {timestampsInSnapshots: true}
+  FIRESTORE_SETTINGS: {timestampsInSnapshots: true},
+  CREDENTIALS_FOR_TESTING: {
+    AWS_ACCESS_KEY_ID: 'AKIAJUIA43FVXDT2DGKQ',
+    AWS_SECRET_ACCESS_KEY: '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',
+    AWS_DEV_ID: 839160517611
+  }
 }
+// try to wrap these info cloud funtions environment variables
