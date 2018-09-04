8eb23baf68e1e64303d89cd74a208688416b085e
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-09-04 21:06:36 +0530

    ready for exporting

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,15 @@
 'use strict'
-function exportFunction () {
+module.exports = (app) => {
   const glob = require('glob')
   const path = require('path')
-  const FIRESTORE_TRIGGER_PATH = path.join(__dirname, './exports/firestore')
-  const HTTP_TRIGGER_PATH = path.join(__dirname, './exports/http')
-  const RTDB_TRIGGER_PATH = path.join(__dirname, './exports/Rtdb')
-  const AUTH_TRIGGER_PATH = path.join(__dirname, './exports/Auth')
-  const ANALYTICS_TRIGGER_PATH = path.join(__dirname, './exports/Analytics')
-  const CRASHLYTICS_TRIGGER_PATH = path.join(__dirname, './exports/Crashlytics')
-  const PUB_SUB_TRIGGER_PATH = path.join(__dirname, './exports/PubSub')
-  const STORAGE_TRIGGER_PATH = path.join(__dirname, './exports/Storage')
+  const FIRESTORE_TRIGGER_PATH = path.resolve(__dirname, './exports/firestore')
+  const HTTP_TRIGGER_PATH = path.resolve(__dirname, './exports/http')
+  const RTDB_TRIGGER_PATH = path.resolve(__dirname, './exports/Rtdb')
+  const AUTH_TRIGGER_PATH = path.resolve(__dirname, './exports/Auth')
+  const ANALYTICS_TRIGGER_PATH = path.resolve(__dirname, './exports/Analytics')
+  const CRASHLYTICS_TRIGGER_PATH = path.resolve(__dirname, './exports/Crashlytics')
+  const PUB_SUB_TRIGGER_PATH = path.resolve(__dirname, './exports/PubSub')
+  const STORAGE_TRIGGER_PATH = path.resolve(__dirname, './exports/Storage')
   const EXPORTS_FOLDER = [
     FIRESTORE_TRIGGER_PATH,
     HTTP_TRIGGER_PATH,
@@ -26,17 +26,16 @@ function exportFunction () {
       const only = process.env.FUNCTION_NAME
       const name = functionNameGenerator(file)
       if (only === undefined || only === name) {
-        console.log(name + '' + 'file: ' + element + file)
+        console.log(name + '' + 'file: ' + path.resolve(element, file))
+        exports[name] = require(path.resolve(element, file))
       }
     })
   }
+  function functionNameGenerator (file) {
+    const camel = require('camelcase')
+    const split = file.split('/')
+    const event = split.pop().split('.')[0]
+    const snake = `${split.join('_')}${event}`
+    return camel(snake)
+  }
 }
-
-function functionNameGenerator (file) {
-  const camel = require('camelcase')
-  const split = file.split('/')
-  const event = split.pop().split('.')[0]
-  const snake = `${split.join('_')}${event}`
-  return camel(snake)
-}
-exportFunction()
