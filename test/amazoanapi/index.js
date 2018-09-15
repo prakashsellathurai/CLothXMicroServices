504f825e892993fc0423cff55329d20b99924f27
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-09-15 22:50:05 +0530

    need to test at scratch pad and essentials must be decided

--- test/amazoanapi/index.js
+++ test/amazoanapi/index.js
@@ -2,17 +2,6 @@
 const CONSTANTS = require('../../functions/shared/environment/CONSTANTS')
 const ACCESS_KEY_ID = CONSTANTS.CREDENTIALS_FOR_TESTING.AWS_ACCESS_KEY_ID
 const ACCESS_SECRET_KEY = CONSTANTS.CREDENTIALS_FOR_TESTING.AWS_SECRET_ACCESS_KEY
-var amazonMWS = require('amazon-mws')(ACCESS_KEY_ID, ACCESS_SECRET_KEY)
-
-amazonMWS.feeds.search({
-  'Version': '2009-01-01',
-  'Action': 'GetFeedSubmissionList',
-  'SellerId':ACCESS_KEY_ID,
-  'MWSAuthToken': ACCESS_SECRET_KEY
-}, function (error, response) {
-  if (error) {
-    console.log('error ', error)
-    return
-  }
-  console.log('response', response)
-})
+const DEVELOPER_ID = CONSTANTS.CREDENTIALS_FOR_TESTING.AWS_DEV_ID
+var amazonMWS = require('./node_modules/amazon-mws/lib/amazon-mws')(ACCESS_KEY_ID, ACCESS_SECRET_KEY)
+amazonMWS
