8bcf9cde9bebc56b9567ff6f9feffaa5dd803a89
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-09-20 20:39:12 +0530

    unwanted imports removed

--- functions/exports/Firestore/Oncreate/Store.export.js
+++ functions/exports/Firestore/Oncreate/Store.export.js
@@ -1,8 +1,6 @@
 //= ===================================== IMPORTS ===============================================//
 var functions = require('firebase-functions')
 var dbFun = require('../../../shared/firestore/CRUD/db')
-var sendEmail = require('../../../shared/utils/Mail/sendmail')
-var sendMessage = require('../../../shared/utils/message/SendMessage')
 function WebsiteHandler (snap, context) {
   let storeId = context.params.storeId
   let registerUid = snap.data().registerUid
