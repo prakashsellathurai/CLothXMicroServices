7af56105bfba43c6a8c781c0e9ffc9f7b14fa900
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-10-12 18:18:52 +0530

    update dpaclage for rrequest promise

---
+++ functions/shared/utils/integrations/flipkart/Get_access_token.js
@@ -0,0 +1,25 @@
+let ERROR_MESSAGE = {
+  error: 'key/secret is missing'
+}
+module.exports = function FlipkartGetAccessToken (key, secret) {
+  let FLIPKART_API_BASE_ROOT = 'https://api.flipkart.net/oauth-service'
+
+  var request = require('request-promise')
+  if (key == null || secret == null) {
+    return Promise.resolve(ERROR_MESSAGE)
+  } else {
+    return request
+      .get({
+        url: FLIPKART_API_BASE_ROOT + '/oauth/token',
+        headers: {},
+        qs: {
+          grant_type: 'client_credentials',
+          scope: 'Seller_Api',
+          'client_id': `${key}`,
+          'client_secret': `${secret}`
+        }
+      })
+      .then((res) => res)
+      .catch((err) => JSON.parse(JSON.stringify(err)).response.body)
+  }
+}
