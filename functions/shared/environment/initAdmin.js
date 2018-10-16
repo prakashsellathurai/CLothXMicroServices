4b991365a84e2b2efdcb9b7edb0f0e548717a281
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-10-17 00:51:02 +0530

    env configured

--- functions/shared/environment/initAdmin.js
+++ functions/shared/environment/initAdmin.js
@@ -1,20 +1,24 @@
 'use strict'
 var admin = require('firebase-admin')
-var serviceAccount = require('./clothxnet-firebase-adminsdk-wkk1h-a27faaab6d.json')
-var env = require('./env')
-function CredentialsForProduction () {
+var ProductionEnvServiceAccount = require('./clothxnet-firebase-adminsdk-wkk1h-a27faaab6d.json')
+var TestEnvServiceAccount = require('./clothxtest-firebase-adminsdk-0bpps-e18156c08d.json')
+var path = require('path')
+function setCredentials () {
   try {
+    var fs = require('fs')
+    let deploymentProjectConfig = JSON.parse(fs.readFileSync(path.join(__dirname, '/.deployenv'), 'utf8'))
+    let serviceAccount = (deploymentProjectConfig.production) ? ProductionEnvServiceAccount : TestEnvServiceAccount
+    let storageBucket = deploymentProjectConfig.storage.bucket
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
-      storageBucket: env.STORAGE_BUCKET
+      storageBucket: storageBucket
     })
-    const firestore = admin.firestore()
-    firestore.settings(env.FIRESTORE_SETTINGS)
+
     return admin
   } catch (e) {
     console.error(e)
   }
 }
 module.exports = {
-  CredentialsForProduction: CredentialsForProduction
+  setCredentials: setCredentials
 }
