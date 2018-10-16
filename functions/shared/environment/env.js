248d72f5894d80f3d67a7c776d825f92b11d2467
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-10-17 00:51:02 +0530

    env configured

--- functions/shared/environment/env.js
+++ functions/shared/environment/env.js
@@ -5,7 +5,6 @@ module.exports = {
   CLOTH_WILDCARD: 'user/{userId}/clothes/{clothId}',
   SECRET_TOKEN: 'qbjbEmQT64UiHe3FXHR',
   MSG_91_API_KEY: '187762AxiHe71B5b2bf557',
-  STORAGE_BUCKET: 'clothxnet.appspot.com',
   SIZE_ARRAY_OBJECT: { 'S': 0, 'M': 1, 'L': 2, 'XL': 3, '2XL': 4, '3XL': 5 },
   GET_SIGNED_URL_SETTINGS: {
     action: 'read',
--- functions/shared/utils/integrations/flipkart/router/router.js
+++ functions/shared/utils/integrations/flipkart/router/router.js
@@ -2,11 +2,20 @@
 const express = require('express')
 const flipkartRouter = express.Router()
 const getAccesToken = require('../Get_access_token')
-
+const db = require('../../../../firestore/CRUD/db')
 flipkartRouter.post('/accesstoken', (req, res) => {
   let clientid = req.query.client_id
   let clientSecret = req.query.client_secret
+  let storeId = req.query.store_id
+
   return getAccesToken(clientid, clientSecret)
+    .then((response) => {
+      if (!response.error) {
+        return db.saveFlipkartAccessTokenCredentials(storeId, clientid, clientSecret, response.access_token)
+          .then(() => response)
+      }
+      return response
+    })
     .then((response) => res.json(response))
 })
 module.exports = flipkartRouter
