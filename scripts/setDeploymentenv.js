37f5caf365c93f02ba6a40425d010b43456dfb7d
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-10-18 10:56:58 +0000

    Update scripts/setDeploymentenv.js

--- scripts/setDeploymentenv.js
+++ scripts/setDeploymentenv.js
@@ -12,11 +12,11 @@ process
     var path = require('path')
     var json = JSON.stringify(obj)
     var fs = require('fs')
-    fs.writeFile(path.join(__dirname, './../functions/shared/environment/.deployenv'), json, 'utf8', (err, data) => {
-      if (err) {
-        console.error('error writing .dotenv file' + err)
-      } else {
-        console.log('\x1b[32m', '\t ✓✓✓✓ environment set sucessfully')
-      }
-    })
+    fs.writeFile(
+      path.join(__dirname, './../functions/shared/environment/.deployenv')
+    , json
+    , 'utf8', 
+    (err, data) => (err) ?
+     console.error('error writing .dotenv file' + err) :
+     console.log('\x1b[32m', '\t ✓✓✓✓ environment set sucessfully'))
   })
