d1681843120c4d7ced38420122a269ffa4f7f47b
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-10-19 12:33:06 +0530

    api end points intiated

--- functions/shared/utils/integrations/flipkart/router/router.js
+++ functions/shared/utils/integrations/flipkart/router/router.js
@@ -3,7 +3,8 @@ const express = require('express')
 const flipkartRouter = express.Router()
 const getAccesToken = require('../Get_access_token')
 const db = require('../../../../firestore/CRUD/db')
-flipkartRouter.post('/accesstoken', (req, res) => {
+// route HAndlers
+function accesstokenRouteHandler (req, res) {
   let clientid = req.query.client_id
   let clientSecret = req.query.client_secret
   let storeId = req.query.store_id
@@ -17,5 +18,23 @@ flipkartRouter.post('/accesstoken', (req, res) => {
     .then((response) => db.LogOnflipkartAccessTokenTrigger(storeId, response))
     .then((response) => res.json(response))
     .catch(() => res.json({error: 'server error', error_description: 'server responded with status 500'}))
-  })
+}
+
+// routes
+flipkartRouter.post('/accesstoken', (req, res) => accesstokenRouteHandler(req, res) )
+flipkartRouter.post('/createlistings', (req, res) => {
+
+})
+flipkartRouter.post('/listings/update', (req, res) => {
+
+})
+flipkartRouter.get('/listings/{sku-ids}', (req, res) => {
+
+})
+flipkartRouter.post('/listings/update/price', (req, res) => {
+
+})
+flipkartRouter.post('/listings/update/inventory', (req, res) => {
+
+})
 module.exports = flipkartRouter
