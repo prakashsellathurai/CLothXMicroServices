8d4cd33c9df46856f536a8d7ab66bbcf9ff3e81a
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-10-19 14:13:27 +0000

    Update functions/shared/utils/integrations/flipkart/router/router.js

--- functions/shared/utils/integrations/flipkart/router/router.js
+++ functions/shared/utils/integrations/flipkart/router/router.js
@@ -7,14 +7,16 @@ flipkartRouter.post('/accesstoken', (req, res) => {
   let clientid = req.query.client_id
   let clientSecret = req.query.client_secret
   let storeId = req.query.store_id
-
-  return getAccesToken(clientid, clientSecret)
+   return (!clientid) ? res.json({error: 'unknown query params', error_description:'client_id not provided' })
+  : (!clientSecret) ? res.json({error: 'unknown query params', error_description:'client_secret not provided'})   : (!storeId) ? res.json({error: 'unknown query params', error_description:'store_id not provided' })
+  : getAccesToken(clientid, clientSecret)
     .then((response) => (!response.error)
       ? db.saveFlipkartAccessTokenCredentials(storeId, clientid, clientSecret, response.access_token)
-        .then(() => response)
+      .then(() => response)
       : response)
     .then((response) => db.LogOnflipkartAccessTokenTrigger(storeId, response))
     .then((response) => res.json(response))
     .catch(() => res.json({error: 'server error', error_description: 'server responded with status 500'}))
-})
+  }
+ })
 module.exports = flipkartRouter
