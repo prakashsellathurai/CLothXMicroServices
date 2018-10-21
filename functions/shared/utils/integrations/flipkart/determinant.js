dc23b2bd10797f84af2f2cf7d22ae57bcafd1979
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-10-21 22:02:57 +0530

    determinant.js added need to configure

---
+++ functions/shared/utils/integrations/flipkart/determinant.js
@@ -0,0 +1,25 @@
+'use strict'
+const VALIDITY_STATUS_OK = 'OK'
+function createlistings (obj) {
+
+}
+function updatelistings (obj) {
+
+}
+function listingsViaSKU (obj) {
+
+}
+function updatelistingsprice (obj) {
+
+}
+function inventorylistings (obj) {
+
+}
+module.exports = {
+  createlistings: createlistings,
+  updatelistings: updatelistings,
+  listingsViaSKU: listingsViaSKU,
+  updatelistingsprice: updatelistingsprice,
+  inventorylistings: inventorylistings,
+  VALIDITY_STATUS_OK: VALIDITY_STATUS_OK
+}
