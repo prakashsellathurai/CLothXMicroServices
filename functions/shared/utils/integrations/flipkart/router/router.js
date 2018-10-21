08926cb95bc692d088a8c4ea43950fe563bd95e8
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-10-21 22:04:05 +0530

    router .js updated

--- functions/shared/utils/integrations/flipkart/router/router.js
+++ functions/shared/utils/integrations/flipkart/router/router.js
@@ -3,38 +3,63 @@ const express = require('express')
 const flipkartRouter = express.Router()
 const getAccesToken = require('../Get_access_token')
 const db = require('../../../../firestore/CRUD/db')
+const createlisting = require('./../create_listings')
 // route HAndlers
+
 function accesstokenRouteHandler (req, res) {
   let clientid = req.query.client_id
   let clientSecret = req.query.client_secret
   let storeId = req.query.store_id
-   return (!clientid) ? res.json({error: 'unknown query params', error_description:'client_id not provided' })
-  : (!clientSecret) ? res.json({error: 'unknown query params', error_description:'client_secret not provided'})   : (!storeId) ? res.json({error: 'unknown query params', error_description:'store_id not provided' })
-  : getAccesToken(clientid, clientSecret)
-    .then((response) => (!response.error)
-      ? db.saveFlipkartAccessTokenCredentials(storeId, clientid, clientSecret, response.access_token)
-      .then(() => response)
-      : response)
-    .then((response) => db.LogOnflipkartAccessTokenTrigger(storeId, response))
-    .then((response) => res.json(response))
-    .catch(() => res.json({error: 'server error', error_description: 'server responded with status 500'}))
+  if (storeId == null) {
+    res.json({
+      error: 'unauthorized',
+      error_description: 'store_id not given'
+    })
+  } else if (clientid == null) {
+    res.json({
+      error: 'unauthorized',
+      error_description: 'client_id not given'
+    })
+  }
+  else if (clientSecret == null) {
+    res.json({
+      error: 'unauthorized',
+      error_description: 'client_secret not given'
+    })
+  } else {
+    return getAccesToken(clientid, clientSecret)
+      .then((response) => (!response.error)
+        ? db.saveFlipkartAccessTokenCredentials(storeId, clientid, clientSecret, response.access_token)
+          .then(() => response)
+        : response)
+      .then((response) => db.LogOnflipkartAccessTokenTrigger(storeId, response))
+      .then((response) => res.json(response))
+      .catch((err) => {
+        console.log(err)
+        res.json({error: 'server error', error_description: 'server responded with status 500'})
+      })
+  }
 }
 
 // routes
-flipkartRouter.post('/accesstoken', (req, res) => accesstokenRouteHandler(req, res) )
-flipkartRouter.post('/createlistings', (req, res) => {
+flipkartRouter
+  .post('/accesstoken', (req, res) => accesstokenRouteHandler(req, res))
+flipkartRouter
+  .post('/createlistings', (req, res) => createlisting(req.query.Authorization, req.body)
+    .then((response) => res.json(response)))
+flipkartRouter
+  .post('/listings/update', (req, res) => {
+  })
+flipkartRouter
+  .get('/listings/{sku-ids}', (req, res) => {
 
-})
-flipkartRouter.post('/listings/update', (req, res) => {
+  })
+flipkartRouter
+  .post('/listings/update/price', (req, res) => {
 
-})
-flipkartRouter.get('/listings/{sku-ids}', (req, res) => {
+  })
 
-})
-flipkartRouter.post('/listings/update/price', (req, res) => {
+  .post('/listings/update/inventory', (req, res) => {
 
-})
-flipkartRouter.post('/listings/update/inventory', (req, res) => {
-
-})
+  })
 module.exports = flipkartRouter
