a5fc5ccccac5dee20123086dc84218bb7df2d6a8
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-10-21 22:03:46 +0530

    get access token params verified before get into the request header

--- functions/shared/utils/integrations/flipkart/Get_access_token.js
+++ functions/shared/utils/integrations/flipkart/Get_access_token.js
@@ -1,25 +1,13 @@
 let ERROR_MESSAGE = {
-  error: 'unauthorized error',
+  error: 'unauthorized',
   error_description: 'query params not given'
 }
-const env = require('./../../../environment/env')
-let FLIPKART_API_BASE_ROOT = env.OMNI_CHANNEL_INTEGRATION.FLIKART.AUTH_API_URL
+const apiRequest = require('./Api_request')
 module.exports = (key, secret) => {
-  var request = require('request-promise')
   if (key == null || secret == null) {
     return Promise.resolve(ERROR_MESSAGE)
   } else {
-    return request
-      .get({
-        url: FLIPKART_API_BASE_ROOT,
-        headers: {},
-        qs: {
-          grant_type: 'client_credentials',
-          scope: 'Seller_Api',
-          'client_id': `${key}`,
-          'client_secret': `${secret}`
-        }
-      })
+    return apiRequest.GetAccessToken(key, secret)
       .then((res) => JSON.parse(JSON.stringify(res)))
       .catch((err) => JSON.parse(JSON.stringify(err)).response.body)
   }
