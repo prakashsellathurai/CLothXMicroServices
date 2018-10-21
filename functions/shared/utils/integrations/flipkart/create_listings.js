4ed961f579f8a49271eca462b71bb20729e3ad0f
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-10-21 22:03:14 +0530

    createlistings updated

--- functions/shared/utils/integrations/flipkart/create_listings.js
+++ functions/shared/utils/integrations/flipkart/create_listings.js
@@ -1,6 +1,9 @@
+'use strict'
 const URL = require('./URL_generator').createListing()
-const request = require('request-promise')
-
+const API_REQUEST = require('./Api_request')
+// const determinant = require('./determinant')
+// const STATUS_OK = determinant.VALIDITY_STATUS_OK
 module.exports = (PostedListingObj) => {
-
+  return API_REQUEST.post(URL, PostedListingObj)
+    .then((response) => JSON.parse(response))
 }
