c83595cc08225984ddc1c2e2cd09b3135aebf31f
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-10-27 19:07:56 +0530

    added onwrite store.export.js

--- functions/exports/Firestore/onCreate/Store.export.js
+++ functions/exports/Firestore/onCreate/Store.export.js
@@ -1,12 +1,18 @@
 //= ===================================== IMPORTS ===============================================//
-var functions = require('firebase-functions')
-var dbFun = require('../../../shared/firestore/CRUD/db')
-
+const functions = require('firebase-functions')
+const dbFun = require('../../../shared/firestore/CRUD/db')
+const razorpayApi = require('./../../../shared/utils/payment/razorpay')
 function OnCreateStoreHandler (snap, context) {
   let storeId = context.params.storeId
   let registerUid = snap.data().registerUid
-
-  return dbFun.AssociateStoreInfoToUser(registerUid, storeId)
+  let contactNo = snap.data().contactNo
+  let notes = snap.data().description
+  let name = snap.data().usn
+  let email = registerUid
+  return dbFun
+    .AssociateStoreInfoToUser(registerUid, storeId)
+    .then(() => razorpayApi
+      .CreateCustomer(name, email, contactNo, notes))
 }
 
 // ==================================================================================================
