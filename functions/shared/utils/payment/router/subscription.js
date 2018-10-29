fc02670b2537b8955b257adf3dac5f68bd710d55
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-10-29 21:42:23 +0530

    updated subscription.js

--- functions/shared/utils/payment/router/subscription.js
+++ functions/shared/utils/payment/router/subscription.js
@@ -1,3 +1,39 @@
 'use strict'
 const express = require('express')
 const subscriptionRouter = express.Router()
+const razorpayApi = require('./../razorpay')
+const db = require('./../../../firestore/CRUD/db')
+subscriptionRouter.post('/verify', (req, res) => {
+  let storeId = req.query.store_id
+  let razorpayPaymentId = req.body.razorpay_payment_id
+  let razorpaySubscriptionId = req.body.razorpay_subscription_id
+  let razorpaySignature = req.body.razorpay_signature
+  if (razorpayPaymentId == null) {
+    res.json({error: 'unauthorized', error_description: 'razorpay_payment_id is required'})
+  } else if (razorpaySubscriptionId == null) {
+    res.json({error: 'unauthorized', error_description: 'razorpay_subscription_id is required'})
+  } else if (razorpaySignature == null) {
+    res.json({error: 'unauthorized', error_description: 'razorpay_signature is required'})
+  } else if (storeId == null) {
+    res.json({error: 'unauthorized', error_description: 'store_id is required'})
+  } else if (razorpayApi
+    .verifyAuthTransaction(razorpayPaymentId, razorpaySubscriptionId, razorpaySignature)) {
+    return db
+      .logPaymentAuthVerification(storeId, razorpaySubscriptionId, razorpaySignature)
+      .then((docRef) => {
+        let docId = docRef.id
+        res.json({doc_id: docId})
+      })
+  } else {
+    res.json({error: 'unauthorized', error_description: 'credentials not valid'})
+  }
+})
+subscriptionRouter.post('/create', (req, res) => {
+  let params = req.body
+  return razorpayApi
+    .CreateSubscription(params)
+    .then((response) => res
+      .json(JSON
+        .parse(response)))
+})
+module.exports = subscriptionRouter
