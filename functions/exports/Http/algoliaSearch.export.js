6470363d18314b5275a1b8420740fc7206f337b0
Pon Muthu Selvam N <nponmuthuselvam@gmail.com>
2018-11-12 23:25:17 +0530

    algolia onUpdate, search api completed

--- functions/exports/Http/algoliaSearch.export.js
+++ functions/exports/Http/algoliaSearch.export.js
@@ -3,18 +3,35 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const algoliaClient = require('../../shared/environment/initAlgoliaClient').withCredentials()
-app.use(cors({ origin: true }))
+app.use(cors({origin: true}))
 app.post('/product_search', (req, res) => {
-  const query = JSON.parse(req.query)
-  let index = algoliaClient.initIndex('product_search')
-  return index
-    .search(query)
-    .then((response) => {
-      res.json(response.hits)
-    })
+    const query = JSON.parse(req.body.query)
+    const filters = 'data.isListable:true AND data.isDeleted:false'
+    let index = algoliaClient.initIndex('product_search')
+    return index
+        .search({query: query, filters: filters})
+        .then((response) => {
+            res.json(response.hits)
+        })
 })
-app.post('/all', (req, res) => {
-
+app.post('/store_search', (req, res) => {
+    const query = JSON.parse(req.body.query)
+    const filters = `data.storeId:${req.body.storeId} AND data.isListable:true AND data.isDeleted:false`
+    let index = algoliaClient.initIndex('product_search')
+    return index
+        .search({query: query, filters: filters})
+        .then((response) => {
+            res.json(response.hits)
+        })
 })
-
+app.post('/store_search_all', (req, res) => {
+    const query = JSON.parse(req.body.query)
+    const filters = `data.storeId:${req.body.storeId} AND data.isDeleted:false`
+    let index = algoliaClient.initIndex('product_search')
+    return index
+        .search({query: query, filters: filters})
+        .then((response) => {
+            res.json(response.hits)
+        })
+})
 module.exports = functions.https.onRequest(app)
