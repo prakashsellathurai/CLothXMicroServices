5e8e34d714a30aa87627e0fcf1035523ab5eb229
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-11-27 17:15:10 +0530

    Revert "Revert "merge""
    This reverts commit 04d9d50eaf0fca9b1e9c61065d08d6e5a2e0f82a.

--- functions/exports/Firestore/onCreate/Invoice.export.js
+++ functions/exports/Firestore/onCreate/Invoice.export.js
@@ -1,16 +1,15 @@
 //= ===================================== IMPORTS ===============================================//
 var functions = require('firebase-functions')
-var db = require('../../../shared/firestore/CRUD/index')
+var db = require('./../../../shared/firestore/CRUD/index')
 var sendMessage = require('./../../../shared/utils/message/SendMessage')
 var utils = require('./../../../shared/utils/general_utils')
 
+// ===============================================================================================
 function MainHandler (snap, context) {
   const cartProducts = snap.data().cartProducts
   const sendSmsBoolean = snap.data().sendSms
   const cartProducts = snap.data().cartProducts
   const invoiceId = context.params.invoiceId
-  const customerNo = snap.data().customerNumber
-  const storeId = snap.data().storeUid
   return db
     .reduce
     .productsOnLocalInventory(cartProducts)
@@ -30,7 +29,21 @@ function MainHandler (snap, context) {
       return db
         .update
         .customerReward(givenCustomerData)
-    })
+    }).then(() => {
+      let Message = `https://www.spoteasy.in/u/invoice/${invoiceId}`
+      if (sendSmsBoolean) {
+        return sendMessage(customerNo, Message)
+          .then((body) => JSON.parse(body))
+          .then((body) => {
+            let messageSuccess = (body.type === 'success')
+            let smsId = (messageSuccess) ? body.message : utils.generateId()
+            let status = body.type
+            let errorDescription = body.message
+            return db.utils.sms.log.OnInvoiceReport(storeId, smsId, customerNo, status, errorDescription)
+          })
+      } else {
+        return Promise.resolve(0)
+      } })
          .catch((err) => {
       if (err) {
         return dbFun.SetInvoicePendingStatusToFalse(invoiceId)
