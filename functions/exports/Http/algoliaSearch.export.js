47c7fa43fd9a069e83425b08040b30e54a0e3213
Pon Muthu Selvam N <nponmuthuselvam@gmail.com>
2018-12-08 02:03:13 +0530

    filter and sort added to algolia search

--- functions/exports/Http/algoliaSearch.export.js
+++ functions/exports/Http/algoliaSearch.export.js
@@ -5,13 +5,24 @@ const app = express()
 const algoliaClient = require('../../shared/environment/initAlgoliaClient').withCredentials()
 app.use(cors({origin: true}))
 app.post('/product_search', (req, res) => {
-    let query = req.body.query
-    console.log(req.body)
+    let index;
+
+    let reqFilters = req.body.filters
+    let reqSortBy = req.body.sortBy
+    const query = req.body.query + `${reqFilters.occasion}`
+
     if (typeof query === 'undefined') {
         res.json({error: 'invaild query', error_description: 'query key is undefined'})
     } else {
-        const filters = `isListable:true AND isDeleted:false`
-        let index = algoliaClient.initIndex('product_search')
+        const filters = `isListable:true AND isDeleted:false AND gender: ${reqFilters.categories.gender} AND sellingPrice>=${reqFilters.price.min} AND sellingPrice<=${reqFilters.price.max}`
+
+        if(reqSortBy === 'high2low') {
+            index = algoliaClient.initIndex('product_price_desc')
+        } else if (reqSortBy === 'low2high'){
+            index = algoliaClient.initIndex('product_price_asc')
+        } else {
+            index = algoliaClient.initIndex('product_search')
+        }
         return index
             .search({query: query, filters: filters})
             .then((response) => {
@@ -20,15 +31,26 @@ app.post('/product_search', (req, res) => {
     }
 })
 app.post('/store_search', (req, res) => {
-    let query = req.body.query
+    let index;
+
+    let reqFilters = req.body.filters
+    let reqSortBy = req.body.sortBy
+    const query = req.body.query + `${reqFilters.occasion}`
+
     let storeId = req.body.storeId
     if (typeof query === 'undefined') {
         res.json({error: 'invaild query', error_description: 'query  is undefined'})
     } else if (typeof storeId === 'undefined') {
         res.json({error: 'invaild storeId', error_description: 'storeid is undefined'})
     } else {
-        const filters = `storeId:${storeId} AND isListable:true AND isDeleted:false`
-        let index = algoliaClient.initIndex('product_search')
+        const filters = `storeId:${storeId} AND isListable:true AND isDeleted:false AND gender: ${reqFilters.categories.gender} AND sellingPrice>=${reqFilters.price.min} AND sellingPrice<=${reqFilters.price.max}`
+        if(reqSortBy === 'high2low') {
+            index = algoliaClient.initIndex('product_price_desc')
+        } else if (reqSortBy === 'low2high'){
+            index = algoliaClient.initIndex('product_price_asc')
+        } else {
+            index = algoliaClient.initIndex('product_search')
+        }
         return index
             .search({query: query, filters: filters})
             .then((response) => {
