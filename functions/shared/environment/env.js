268afaaf75bc0bb1bae39db5d52f52ecbdb5cf21
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-12-07 14:28:18 +0530

    parsed the env.js json

--- functions/shared/environment/env.js
+++ functions/shared/environment/env.js
@@ -8,28 +8,28 @@ module.exports = {
   },
   FIRESTORE: {
     PRODUCTION_CREDENTIALS: {
-      'type': 'service_account',
-      'project_id': 'clothxnet',
-      'private_key_id': 'a27faaab6d55f367a8b88494f106e7983a8dd104',
-      'private_key': '-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCrQ6PSDuBn0qnZ\ntLrnm96ytinbuXZ1Ef8R/BeQVGROoqvd7QNxa1Yu2NpMN8/BkwwYIhHhnpBl2372\n5igsbPDJ/JfA8o06Lq9rYEXOj2QwEBo2DpnvLCFiqhqph58AH+HLEHGNDuMIM850\nW0/QevkVCXCAzzolv1aEVTMYeDut35nhAyV9rvAnwiD7xKh8eBJlbQvpIqvYC6hP\nnZQql7WHDAQ/pi6TreNpjCr0Vnt78WAe9z4iRkvvIsBgY+j3YdHVKxgyGt+awDia\nE4aX+YMBoF4teNBlzxQ0DPV95XvplA06z9VSHgP2x/nLceAGQhkjazMr4jt17fDE\nko2XM3wxAgMBAAECggEAFP9W/3MJelEut91bqPfIRvrkiOblKhdJwYe5z9046xcw\noSXk7SvuxhGaOgwBscb4jaDntlS9eW9+0oCHXmMA5pLMfECApiiBI1YLWPblnqrq\nu8PwhY1Qjosyd6SEDvzZrw7WDShNF31ai5OCZpodHcIpg/FkPKqHzfOwLwjWsEtt\nOxmKQU8fL9yh+X5NNQXjcKRaBf5bf3e0bgVEDMovyOGFtDKMaxy6R2YTt99A/gfI\nU9j4v1emBMZHpIf4gyX535/X3j/GGJtTpri3CLDtStY4KCx/qKU4DVJCUeFvE6LO\nHD1Gi+V/xVkSLlwmrAfs+QZ1ERIyrLFvBJNb0V+MrQKBgQDmaC7YxqP8lH0hgCVa\nkqYa43R0kTMUn6p1REPRI+nEslOndDzRMswA9/qJFoP20/hR3ePVpHPWZHncIJRW\nKwc1AQInixevMQ0veVI5K4Kf1wFHD3Oe+A+idfqhmu4J6lGoEqOSrAWSA8aEmjUf\njrA29Z92WIKKxk1UcQek8W4JbwKBgQC+Sa7R/iTGTUcK0j4TR40Rxpr+0NW0SF4E\nP/ZSlpYXOIeY3Fp3WwwWX1VRAYHR0EB5y6jxYvN0il0M0FfGsgWDIUWJAcNMLucu\n6cCDgpOwnHmXLzRBrEtKSCVDqkM2EKi1KFHlAUdzOoGFRKllPo1b86Lz/c2+rSOI\n/JVAPavEXwKBgQCnXgqZoUHB2kNDMIjx0BEk+xC/FDJcytbRtDLnGwVWwosN2tvK\nwvF5yqiNKHPXuoZpQZuct4cFt5rlkizuovbrQ75C50DfG+yODbzylm6emVnuOoUs\ndtk0lCrpdVdwkGucgV2cz6SEOoXcEQCB2e6bHbUmDIlXMVE/A5Qf9tiNtQKBgQCl\nBXtxq2gy/SOGE+w3hg6mAsm6AFvEfBnkr5+r4eIKA0E85KQj7DagVE7F8Hun/GVd\n+qvpormlMLUffrMQoqQ7IyiNd5TpxadBvP/yTNRODnU4EnzME2I76jM+6jJdebcF\nXBEhe6l+GZfnfjM1lTi8LJ793/aEzNwLWZkJ3DHRGwKBgDrM2PiMWzUIx1eC0XIz\noVLDiR4piI9Z1bwkHqaxICXO+1RYTFeFxPUQXczTU0vrGszjNWDFjxMXHT9mpdqX\nYuQUX2aFJJAcVec5EktR4Vs7+ry4CAbfVyLlyOnc8uxOp277t7g908MpxPARrdH7\ncGg3GJvrygQg6TxkVIwjN03h\n-----END PRIVATE KEY-----\n',
-      'client_email': 'firebase-adminsdk-wkk1h@clothxnet.iam.gserviceaccount.com',
-      'client_id': '108668615371967414413',
-      'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
-      'token_uri': 'https://accounts.google.com/o/oauth2/token',
-      'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
-      'client_x509_cert_url': 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-wkk1h%40clothxnet.iam.gserviceaccount.com'
+      type: 'service_account',
+      project_id: 'clothxnet',
+      private_key_id: 'a27faaab6d55f367a8b88494f106e7983a8dd104',
+      private_key: '-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCrQ6PSDuBn0qnZ\ntLrnm96ytinbuXZ1Ef8R/BeQVGROoqvd7QNxa1Yu2NpMN8/BkwwYIhHhnpBl2372\n5igsbPDJ/JfA8o06Lq9rYEXOj2QwEBo2DpnvLCFiqhqph58AH+HLEHGNDuMIM850\nW0/QevkVCXCAzzolv1aEVTMYeDut35nhAyV9rvAnwiD7xKh8eBJlbQvpIqvYC6hP\nnZQql7WHDAQ/pi6TreNpjCr0Vnt78WAe9z4iRkvvIsBgY+j3YdHVKxgyGt+awDia\nE4aX+YMBoF4teNBlzxQ0DPV95XvplA06z9VSHgP2x/nLceAGQhkjazMr4jt17fDE\nko2XM3wxAgMBAAECggEAFP9W/3MJelEut91bqPfIRvrkiOblKhdJwYe5z9046xcw\noSXk7SvuxhGaOgwBscb4jaDntlS9eW9+0oCHXmMA5pLMfECApiiBI1YLWPblnqrq\nu8PwhY1Qjosyd6SEDvzZrw7WDShNF31ai5OCZpodHcIpg/FkPKqHzfOwLwjWsEtt\nOxmKQU8fL9yh+X5NNQXjcKRaBf5bf3e0bgVEDMovyOGFtDKMaxy6R2YTt99A/gfI\nU9j4v1emBMZHpIf4gyX535/X3j/GGJtTpri3CLDtStY4KCx/qKU4DVJCUeFvE6LO\nHD1Gi+V/xVkSLlwmrAfs+QZ1ERIyrLFvBJNb0V+MrQKBgQDmaC7YxqP8lH0hgCVa\nkqYa43R0kTMUn6p1REPRI+nEslOndDzRMswA9/qJFoP20/hR3ePVpHPWZHncIJRW\nKwc1AQInixevMQ0veVI5K4Kf1wFHD3Oe+A+idfqhmu4J6lGoEqOSrAWSA8aEmjUf\njrA29Z92WIKKxk1UcQek8W4JbwKBgQC+Sa7R/iTGTUcK0j4TR40Rxpr+0NW0SF4E\nP/ZSlpYXOIeY3Fp3WwwWX1VRAYHR0EB5y6jxYvN0il0M0FfGsgWDIUWJAcNMLucu\n6cCDgpOwnHmXLzRBrEtKSCVDqkM2EKi1KFHlAUdzOoGFRKllPo1b86Lz/c2+rSOI\n/JVAPavEXwKBgQCnXgqZoUHB2kNDMIjx0BEk+xC/FDJcytbRtDLnGwVWwosN2tvK\nwvF5yqiNKHPXuoZpQZuct4cFt5rlkizuovbrQ75C50DfG+yODbzylm6emVnuOoUs\ndtk0lCrpdVdwkGucgV2cz6SEOoXcEQCB2e6bHbUmDIlXMVE/A5Qf9tiNtQKBgQCl\nBXtxq2gy/SOGE+w3hg6mAsm6AFvEfBnkr5+r4eIKA0E85KQj7DagVE7F8Hun/GVd\n+qvpormlMLUffrMQoqQ7IyiNd5TpxadBvP/yTNRODnU4EnzME2I76jM+6jJdebcF\nXBEhe6l+GZfnfjM1lTi8LJ793/aEzNwLWZkJ3DHRGwKBgDrM2PiMWzUIx1eC0XIz\noVLDiR4piI9Z1bwkHqaxICXO+1RYTFeFxPUQXczTU0vrGszjNWDFjxMXHT9mpdqX\nYuQUX2aFJJAcVec5EktR4Vs7+ry4CAbfVyLlyOnc8uxOp277t7g908MpxPARrdH7\ncGg3GJvrygQg6TxkVIwjN03h\n-----END PRIVATE KEY-----\n',
+      client_email: 'firebase-adminsdk-wkk1h@clothxnet.iam.gserviceaccount.com',
+      client_id: '108668615371967414413',
+      auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+      token_uri: 'https://accounts.google.com/o/oauth2/token',
+      auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+      client_x509_cert_url: 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-wkk1h%40clothxnet.iam.gserviceaccount.com'
     },
     TEST_CREDENTIALS: {
-      'type': 'service_account',
-      'project_id': 'clothxtest',
-      'private_key_id': 'e18156c08de995aff707552b65641fe575c7bec7',
-      'private_key': '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC+2w0Nw4s5gBVZ\noimtSA1HxqaHKfwtO1rQ23bGKAuNiZ+4nVXyrSLYyvwJJD4lOJdFW3XSUX4EBNKn\nvdctGb/s9CCf/fYyqDmv6XhfBo6D0qXKabaZb+TwaPqkC/ioge0rRdtSstn0ouJT\nAXsQAJzCUSOjxL2XDf5DraVqSn88g3/S8cPMiwcjbohUI0r9SwuhZ6vc7f+TG7gz\n83SR7U5/pxJQJXwkk6IRHL2y63Lezebbb13kPiVG2SfYWY/QSQ/L0ot7wk4meSbW\nZfsSD0izeDkPd9NQYWe/B7amN3GZG7+tlJOQjBZubiqv30i5Ju/UqqJot/nY+41I\n+smPyrRNAgMBAAECggEADusq2+K8p8vSH2sJzKCUtSxwiPaTCUAaAGMfUixVgYSl\nUQuEvo73vzAtalOEbhQR3bCMBwWADwqyIr2NVoUBl8vvdEIoSVvAAqGtMJRfyysf\nSDFiPh/dKp5cyo+gVMpZ19JCliDNNmO4bZpF3IGHv2K99p0Niti5AYukd1NdGxre\nPfNH4dzegFv6s3XD3bnkocoY7o8ric9qufpQgI0GdF6n0CgZBaUlLHPeo/YUtCmu\nty4l8VulMCTvdLvo7fqoasGsUVDBayRctW8hU5RpkwKv8jIlHHVHnkS4fQVZp6vt\nFtdFwiTTGf7wM0ppZ/rnEIqLqzxtU1GrIc8BqwTEGwKBgQD2cJrz94KL1Zuj2uZ+\nKdZvxjtwCdVTT/DyQ6go2EciJ5q03o6xRG99v3K5kVoKfj8/qQTx/rZM7yXVxUzY\nFSgsZlm5hQxaJe2vMWzYQL2aQYeuMCShNk9lwLBzcIJmQy+ewsTKFqVPlMhb271V\nOxyNCOZIL+xg8l1Ok7t8YNBPbwKBgQDGQnBfa6dkZZyRzCBsq9sq4E74VmKiqy2t\nlzmF92N5/VOKnqkD35/w1ZKCANVYX7R/ID/OOaeZp4qyR4TeNxjfdg7qrxahLZbT\nrb445RrBDigha34sWZDPDXlQbpvZdC00XBsbZtU3Q3y0RgfY7/yw9r78j0JjBLOp\nGasI4v+aAwKBgQDCeOf+oBI30RAqRMLhSpi6yd77sfbS8OG9/GAGI2426LPwCTTn\ndI7a16pr/+TjJO86iGGrqgooOgxMXo8eccurBfXGLdUqioW/W6x+FtDImu+8KKev\nRFFxhZZ48o8krd41VZO/uhEn9MwLmSMW0Dfen+hBjPfnJTs2l+Q66ZdZJwKBgHEL\nQPencnKUXYnkUjikZPs1mtGOr/8Ai7MHHw172YSdJR1NCiHjSFcR78+gmgUhzNEP\nmlIEHcRG+5ZboS7zS4Ne1XKLEuQkEnPwBR57w52AbPFhUTgIecEVda6e4s5Cp+PL\nAMnHAJvaJyjDJYZUkEh26L8pEDlVXPt2X0dZ51PxAoGAR9X7gA27dqkwRROeN3o1\nvFOCCDEa/SpnVMqkiiPxuvUusvoWIgOIr6Z3Car0/Eu8lr4WVZ3HSGLRAgRDoo6w\nvNu3/yYuNwmEHdN4jdWl7IgP8lGd9mRRQxh9bgTJ/SEdDBUDWtircKKQCoBvvI0t\n8JaxXN1Vi3QPoryDwjJPzoE=\n-----END PRIVATE KEY-----\n',
-      'client_email': 'firebase-adminsdk-0bpps@clothxtest.iam.gserviceaccount.com',
-      'client_id': '103224230370865167905',
-      'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
-      'token_uri': 'https://oauth2.googleapis.com/token',
-      'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
-      'client_x509_cert_url': 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-0bpps%40clothxtest.iam.gserviceaccount.com'
+      type: 'service_account',
+      project_id: 'clothxtest',
+      private_key_id: 'e18156c08de995aff707552b65641fe575c7bec7',
+      private_key: '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC+2w0Nw4s5gBVZ\noimtSA1HxqaHKfwtO1rQ23bGKAuNiZ+4nVXyrSLYyvwJJD4lOJdFW3XSUX4EBNKn\nvdctGb/s9CCf/fYyqDmv6XhfBo6D0qXKabaZb+TwaPqkC/ioge0rRdtSstn0ouJT\nAXsQAJzCUSOjxL2XDf5DraVqSn88g3/S8cPMiwcjbohUI0r9SwuhZ6vc7f+TG7gz\n83SR7U5/pxJQJXwkk6IRHL2y63Lezebbb13kPiVG2SfYWY/QSQ/L0ot7wk4meSbW\nZfsSD0izeDkPd9NQYWe/B7amN3GZG7+tlJOQjBZubiqv30i5Ju/UqqJot/nY+41I\n+smPyrRNAgMBAAECggEADusq2+K8p8vSH2sJzKCUtSxwiPaTCUAaAGMfUixVgYSl\nUQuEvo73vzAtalOEbhQR3bCMBwWADwqyIr2NVoUBl8vvdEIoSVvAAqGtMJRfyysf\nSDFiPh/dKp5cyo+gVMpZ19JCliDNNmO4bZpF3IGHv2K99p0Niti5AYukd1NdGxre\nPfNH4dzegFv6s3XD3bnkocoY7o8ric9qufpQgI0GdF6n0CgZBaUlLHPeo/YUtCmu\nty4l8VulMCTvdLvo7fqoasGsUVDBayRctW8hU5RpkwKv8jIlHHVHnkS4fQVZp6vt\nFtdFwiTTGf7wM0ppZ/rnEIqLqzxtU1GrIc8BqwTEGwKBgQD2cJrz94KL1Zuj2uZ+\nKdZvxjtwCdVTT/DyQ6go2EciJ5q03o6xRG99v3K5kVoKfj8/qQTx/rZM7yXVxUzY\nFSgsZlm5hQxaJe2vMWzYQL2aQYeuMCShNk9lwLBzcIJmQy+ewsTKFqVPlMhb271V\nOxyNCOZIL+xg8l1Ok7t8YNBPbwKBgQDGQnBfa6dkZZyRzCBsq9sq4E74VmKiqy2t\nlzmF92N5/VOKnqkD35/w1ZKCANVYX7R/ID/OOaeZp4qyR4TeNxjfdg7qrxahLZbT\nrb445RrBDigha34sWZDPDXlQbpvZdC00XBsbZtU3Q3y0RgfY7/yw9r78j0JjBLOp\nGasI4v+aAwKBgQDCeOf+oBI30RAqRMLhSpi6yd77sfbS8OG9/GAGI2426LPwCTTn\ndI7a16pr/+TjJO86iGGrqgooOgxMXo8eccurBfXGLdUqioW/W6x+FtDImu+8KKev\nRFFxhZZ48o8krd41VZO/uhEn9MwLmSMW0Dfen+hBjPfnJTs2l+Q66ZdZJwKBgHEL\nQPencnKUXYnkUjikZPs1mtGOr/8Ai7MHHw172YSdJR1NCiHjSFcR78+gmgUhzNEP\nmlIEHcRG+5ZboS7zS4Ne1XKLEuQkEnPwBR57w52AbPFhUTgIecEVda6e4s5Cp+PL\nAMnHAJvaJyjDJYZUkEh26L8pEDlVXPt2X0dZ51PxAoGAR9X7gA27dqkwRROeN3o1\nvFOCCDEa/SpnVMqkiiPxuvUusvoWIgOIr6Z3Car0/Eu8lr4WVZ3HSGLRAgRDoo6w\nvNu3/yYuNwmEHdN4jdWl7IgP8lGd9mRRQxh9bgTJ/SEdDBUDWtircKKQCoBvvI0t\n8JaxXN1Vi3QPoryDwjJPzoE=\n-----END PRIVATE KEY-----\n',
+      client_email: 'firebase-adminsdk-0bpps@clothxtest.iam.gserviceaccount.com',
+      client_id: '103224230370865167905',
+      auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+      token_uri: 'https://oauth2.googleapis.com/token',
+      auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+      client_x509_cert_url: 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-0bpps%40clothxtest.iam.gserviceaccount.com'
     },
     STOARGE_BUCKET: {
       PRODUCTION: 'clothxnet.appspot.com',
