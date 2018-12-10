3ebcea6389b2b8bb7a58e2b621555d0db67ad341
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-12-10 21:08:20 +0530

    data parser done

--- functions/shared/utils/integrations/algolia/router/shared/algolia_data_parser.js
+++ functions/shared/utils/integrations/algolia/router/shared/algolia_data_parser.js
@@ -8,6 +8,7 @@ const DISTINCT_PRODUCT_PROPERTIES = ['purchasedPrice', 'sellingPrice', 'objectID
 const groupByProductUid = (AlgoliaData) => convertObjToArr(_.mapValues(_.groupBy(AlgoliaData, 'productUid')))
 const convertObjToArr = (obj) => _.values(obj)
 const parseVariants = (v) => _.pick(v, DISTINCT_PRODUCT_PROPERTIES)
+const merger = (arr1, arr2, member) => [...arr1.concat(arr2).reduce((m, o) => m.set(o[member], Object.assign(m.get(o[member]) || {}, o)), new Map()).values()]
 // main functions
 function dataNormalizer (dataArray) {
   let normalizedArray = []
@@ -30,19 +31,22 @@ function makeRequest (_index, _query, _page, _filters) {
       query: _query,
       filters: _filters,
       page: _page,
-      hitsPerPage: RESULTS_PER_PAGE
+      hitsPerPage: 20
     }).then((res) => res.hits)
 }
 function mainEngine (_index, _query, _page, _filters, groupedResults) {
   return makeRequest(_index, _query, _page, _filters)
     .then((results) => {
-      let groupedResults = groupByProductUid(results)
-      if (groupedResults.length < RESULTS_PER_PAGE) {
+      let normalizeddata = merger(groupedResults, dataNormalizer(groupByProductUid(results)), 'productUid')
+      if (normalizeddata.length < RESULTS_PER_PAGE) {
         // do recall here
-        let requiredlength = RESULTS_PER_PAGE - groupedResults.length
+        let requiredlength = RESULTS_PER_PAGE - normalizeddata.length
+        console.log(requiredlength)
+        // return mainEngine(_index, _query, _page + 1, _filters, normalizeddata)
+        return normalizeddata
+      } else {
+        return normalizeddata
       }
-      let normalizeddata = dataNormalizer(groupedResults)
-      return normalizeddata
     })
 }
 module.exports = mainEngine
