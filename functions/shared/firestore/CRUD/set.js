2e4d27504a6c67bc5b189ad64ff87305d87ac44f
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-12-13 16:25:35 +0530

    objectId will be pre defined before adding it to algolia

--- functions/shared/firestore/CRUD/set.js
+++ functions/shared/firestore/CRUD/set.js
@@ -1,6 +1,8 @@
 let admin = require('firebase-admin')
 let firestore = admin.firestore()
 let update = require('./update')
+let utils = require('./../utils/index')
+
 function invoicePendingStatusToFalse (invoiceId) {
   return update.invoicePendingStatus(invoiceId, 'false')
 }
@@ -22,8 +24,36 @@ function objectIDtoProduct (productId, variants) {
       merge: true
     })
 }
+function RandomObjectIdToProduct (productId) {
+  let productDocRef = firestore
+    .doc(`/products/${productId}`)
+  return firestore
+    .runTransaction(t => {
+      return t.get(productDocRef)
+        .then((doc) => {
+          let data = doc.data()
+          let productUid = data.productUid
+          let variants = data.variants
+          for (let index = 0; index < variants.length; index++) {
+            variants[index].objectID = productUid + '_' + index
+          }
+          return utils.prnCheckLoop()
+            .then((prn) => {
+              t.set(doc.ref, {
+                prn: prn,
+                variants: variants,
+                createdOn: admin.firestore.FieldValue.serverTimestamp()
+              }, { merge: true })
+              data.prn = prn
+              data.variants = variants
+              return data
+            })
+        })
+    })
+}
 module.exports = {
   invoicePendingStatusToFalse: invoicePendingStatusToFalse,
   productPRN: productPRN,
-  objectIDtoProduct: objectIDtoProduct
+  objectIDtoProduct: objectIDtoProduct,
+  RandomObjectIdToProduct: RandomObjectIdToProduct
 }
