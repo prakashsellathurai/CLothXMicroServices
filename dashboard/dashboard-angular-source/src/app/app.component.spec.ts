7d7ea95fda0bb38b989f594081e80cf0a297aa52
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-12-16 20:08:02 +0530

    will build client and server

--- dashboard/dashboard-angular-source/src/app/app.component.spec.ts
+++ dashboard/dashboard-angular-source/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'dashboard-angular-app'`, () => {
+  it(`should have as title 'dashboard-angular-source'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('dashboard-angular-app');
+    expect(app.title).toEqual('dashboard-angular-source');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to dashboard-angular-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to dashboard-angular-source!');
   });
 });
--- dashboard/dashboard-angular-source/src/app/app.module.ts
+++ dashboard/dashboard-angular-source/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({appId: 'dashboard'}),
+    BrowserModule.withServerTransition({appId: 'my-app'}),
     AppRoutingModule
   ],
   providers: [],
