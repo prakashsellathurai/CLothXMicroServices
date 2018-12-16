c7e855df55a65be84ff1948f3fe9ee68e36d6754
Prakash Sellathurai <prakash1729brt@gmail.com>
2018-12-16 13:16:28 +0530

    added universal support

--- dashboard/dashboard-angular-app/src/app/app.module.ts
+++ dashboard/dashboard-angular-app/src/app/app.module.ts
@@ -4,12 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({appId: 'dashboard'}),
     AppRoutingModule
   ],
   providers: [],
