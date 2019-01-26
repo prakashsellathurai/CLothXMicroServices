62113b7f39c8e0bfb1e30367588cd49d73d57468
Prakash Sellathurai <prakash1729brt@gmail.com>
2019-01-26 18:52:07 +0530

    resolevd the path directly to reduce

--- functions/shared/firestore/CRUD/reduce.js
+++ functions/shared/firestore/CRUD/reduce.js
@@ -1,3 +1,4 @@
+'use strict'
 let admin = require('firebase-admin')
 let firestore = admin.firestore()
 
--- functions/shared/firestore/CRUD/update.js
+++ functions/shared/firestore/CRUD/update.js
@@ -1,3 +1,4 @@
+'use strict'
 const admin = require('firebase-admin')
 const firestore = admin.firestore()
 const db = require('./index')
