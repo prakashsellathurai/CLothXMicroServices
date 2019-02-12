b85e75f5d6a11d5886f111d3709f5a1761bbdb26
Prakash Sellathurai <prakash1729brt@gmail.com>
2019-02-12 12:13:07 +0530

    test will export at file level

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,12 +1,13 @@
-const db = require('./unit-test/db/db.spec')
-const algolia = require('./unit-test/algolia/algolia.spec')
-const cloudinary = require('./unit-test/cloudinary/init.spec')
-const search = require('./unit-test/HTTPS/algolia/search.spec')
-const razorpay = require('./unit-test/razorpay/init.spec')
-module.exports = {
-  db: db,
-  algolia: algolia,
-  cloudinary: cloudinary,
-  search: search,
-  razorpay: razorpay
-}
+const glob = require('glob')
+const path = require('path')
+const utils = require('./../functions/shared/utils/general_utils')
+let ABSOLUTE_PATH = path.join(__dirname, '/')
+glob
+  .sync('{,!(node_modules)/**/}*.spec.js',
+    { cwd: ABSOLUTE_PATH })
+  .forEach(FILE => {
+    if (FILE !== 'index.spec.js') {
+      var name = utils.functionNameGenerator(FILE)
+      exports[name] = require('./' + FILE)
+    }
+  })
