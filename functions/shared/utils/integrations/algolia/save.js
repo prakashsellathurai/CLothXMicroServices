46085640479b013ec2c2449331ec2a11c4f64db7
Prakash Sellathurai <prakash1729brt@gmail.com>
2019-02-20 09:56:56 +0530

    added customer reward

--- functions/shared/utils/integrations/algolia/save.js
+++ functions/shared/utils/integrations/algolia/save.js
@@ -1,19 +1,5 @@
 'use strict'
 const productIndex = require('./initIndex').product.unsorted
-const utils = require('./utils')
-
-// function product (data) {
-//   data = utils.dataPreprocessor(data)
-//   let variants = utils.extractVariantInProduct(data)
-//   let filteredObject = utils.filterVariantInProduct(data)
-//   let promises = []
-//   for (let index = 0; index < variants.length; index++) {
-//     let variant = variants[index]
-//     let DenormedData = utils.DeNormalizeTheProductData(filteredObject, variant)
-//     promises.push(addProductInalgolia(DenormedData, variant))
-//   }
-//   return Promise.all(promises)
-// }
 
 function product (data) {
   data.objectId = data.productUid
