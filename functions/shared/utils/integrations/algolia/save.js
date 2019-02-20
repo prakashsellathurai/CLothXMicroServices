f40f127364d3a6e89fa6ae0f4ff9256677e1d664
Prakash Sellathurai <prakash1729brt@gmail.com>
2019-02-20 10:09:21 +0530

    updated algolia save.js

--- functions/shared/utils/integrations/algolia/save.js
+++ functions/shared/utils/integrations/algolia/save.js
@@ -2,18 +2,11 @@
 const productIndex = require('./initIndex').product.unsorted
 
 function product (data) {
-  data = utils.dataPreprocessor(data)
-  let variants = utils.extractVariantInProduct(data)
-  let filteredObject = utils.filterVariantInProduct(data)
-  let promises = []
-  for (let index = 0; index < variants.length; index++) {
-    let variant = variants[index]
-    let DenormedData = utils.DeNormalizeTheProductData(filteredObject, variant)
-    promises.push(addProductInalgolia(DenormedData, variant))
-  }
-  return Promise.all(promises)
+  data.objectId = data.productUid
+  return addProductInalgolia(data)
 }
-function addProductInalgolia (DenormedData, variant) {
+
+function addProductInalgolia (DenormedData) {
   if (typeof DenormedData.objectID === 'undefined') {
     console.log(DenormedData)
     console.log('undefined objectId')
